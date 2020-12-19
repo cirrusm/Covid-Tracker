f270f84f170e9177720ef69e63c473c07895df41
Cirrus Mokhtari <Cirrus1994@gmail.com>
2020-12-19 14:36:21 -0800

    Header and cards made

--- src/App.js
+++ src/App.js
@@ -1,10 +1,62 @@
-
-import './App.css';
+import "./App.css";
+import {
+  MenuItem,
+  FormControl,
+  Select,
+  Card,
+  CardContent,
+} from "@material-ui/core";
+import React, { useState, useEffect } from "react";
+import InfoBox from "./Components/InfoBox";
+import Map from "./Components/Map";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("worldwide");
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+          setCountries(countries);
+        });
+    };
+    //Now call the function you just created
+    getCountriesData();
+  }, []);
+
+  const onCountryChange = (event) => {
+    const countryCode = event.target.value;
+    setCountry(countryCode);
+  };
   return (
     <div className="App">
-      
+      <div className="app__header">
+        <h1>Covid Tracker</h1>
+        <FormControl>
+          <Select variant="outlined" onChange={onCountryChange} value={country}>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+      <div className="app__stats">
+        <InfoBox title="Cases" total={2000} cases={1234} />
+        <InfoBox title="Recovered" total={3000} cases={1234} />
+        <InfoBox title="Deaths" total={4000} cases={1234} />
+      </div>
+      <Map />
+
+      {/* Table */}
+      {/* Graph */}
+
+      {/* Map */}
     </div>
   );
 }
---
+++ src/Components/InfoBox.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react';
+import {Card, CardContent, Typography} from "@material-ui/core"
+
+const InfoBox = ({ title, cases, total}) => {
+  return (
+    <div>
+      <Card classname="infoBox">
+        <CardContent>
+          <Typography className="infoBox__title" color="textSecondary">
+            {title}
+          </Typography>
+
+          <h2 className="infoBox__cases">{cases}</h2>
+          <Typography className="infoBox__total" color ="textSecondary">
+            Total: {total}
+          </Typography>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
+
+export default InfoBox;
